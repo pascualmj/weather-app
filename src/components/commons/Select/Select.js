5781032cbccddc415bef1fc1be3c1b59c095e21b
Manuel Pascual <manupascual90@gmail.com>
2020-07-04 13:51:18 -0300

    Add click outside handler

--- src/components/commons/Select/Select.js
+++ src/components/commons/Select/Select.js
@@ -1,25 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import PropTypes from "prop-types";
 import "./select.scss";
 
 import Text from "../Text";
 
+import { useOutsideListener } from "../../../hooks";
+
 const Select = ({ options, value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const wrapperElement = useRef(null);
+  useOutsideListener(wrapperElement, handleClose);
+
+  function handleClose() {
+    setIsOpen(false);
+  }
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
   const handleClick = (value) => {
-    return () => {
+    return (e) => {
       onChange(value);
       setIsOpen(false);
     };
   };
 
   return (
-    <div className="select">
+    <div className="select" ref={wrapperElement}>
       <div className="option-selected" onClick={handleToggle}>
         <Text>{options.find((opt) => opt.value === value).label}</Text>
         <span className="material-icons">unfold_more</span>
