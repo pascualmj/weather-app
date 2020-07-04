08daf53b196bfcbbda09a1cb154dddb4f56d0729
Manuel Pascual <manupascual90@gmail.com>
2020-07-04 19:36:52 -0300

    Include WeatherToday component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import CitySelection from "../components/CitySelection";
+import WeatherToday from "../components/WeatherToday";
 import WeatherForecast from "../components/WeatherForecast";
 
 const Home = () => {
   return (
     <>
       <CitySelection />
-      <WeatherForecast />
+      <WeatherToday className="mt-6" />
+      <WeatherForecast className="mt-9" />
     </>
   );
 };
