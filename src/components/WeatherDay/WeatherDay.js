3b37a28f2ba2d49e98297df7f0053569d78b55e7
Manuel Pascual <manupascual90@gmail.com>
2020-07-04 18:14:19 -0300

    Create WeatherDay component

---
+++ src/components/WeatherDay/WeatherDay.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./weatherDay.scss";
+
+import Text from "../commons/Text";
+import WeatherIcon from "../WeatherIcon";
+import WeatherMinMax from "../WeatherMinMax";
+
+const WeatherDay = ({
+  day,
+  iconId,
+  min,
+  max,
+  description = "",
+  className = "",
+}) => {
+  return (
+    <article className={`weather-day ${className}`}>
+      <Text weight="bold" uppercase>
+        {day}
+      </Text>
+      <WeatherIcon iconId={iconId} description={description} />
+      <WeatherMinMax min={min} max={max} />
+    </article>
+  );
+};
+
+WeatherDay.propTypes = {
+  day: PropTypes.string.isRequired,
+  iconId: PropTypes.string.isRequired,
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  description: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default WeatherDay;
