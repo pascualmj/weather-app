cc282a36248ccfbd77d5153fbab781ca9e5a0f7a
Manuel Pascual <manupascual90@gmail.com>
2020-07-04 18:15:15 -0300

    Include WeatherForecast component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import CitySelection from "../components/CitySelection";
+import WeatherForecast from "../components/WeatherForecast";
 
 const Home = () => {
   return (
     <>
       <CitySelection />
+      <WeatherForecast />
     </>
   );
 };
