2e76839d065dde1d99a149fa133e5df7d33fb1d9
Manuel Pascual <manupascual90@gmail.com>
2020-07-04 13:14:00 -0300

    Create config/constants file

---
+++ src/config/constants.js
@@ -0,0 +1,23 @@
+export const SELECT_OPTIONS_CITIES = [
+  { label: "Current Location", value: "current" },
+  {
+    label: "Rosario",
+    value: "Rosario, Santa Fe, Argentina",
+  },
+  {
+    label: "Córdoba",
+    value: "Ciudad de Córdoba, Córdoba, Argentina",
+  },
+  {
+    label: "Paraná",
+    value: "Paraná, Entre Ríos, Argentina",
+  },
+  {
+    label: "Mendoza",
+    value: "Mendoza, Mendoza, Argentina",
+  },
+  {
+    label: "Misiones",
+    value: "Ciudad de Misiones, Misiones, Argentina",
+  },
+];
