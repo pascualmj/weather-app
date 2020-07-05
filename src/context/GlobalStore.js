0cdae20d41b679ce5e22c7ab94fd1f8ba8372a23
Manuel Pascual <manupascual90@gmail.com>
2020-07-05 14:29:24 -0300

    Add city selection functionality to context

--- src/context/GlobalStore.js
+++ src/context/GlobalStore.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { getLocation } from "../services/geolocationService";
 import { getWeather } from "../services/weatherService";
+
+import { SELECT_OPTIONS_CITIES } from "../config/constants";
 
 export const GlobalContext = React.createContext(null);
 
@@ -8,6 +10,7 @@ const GlobalStore = ({ children }) => {
   const [location, setLocation] = useState(null);
   const [weather, setWeather] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [selectedCity, setSelectedCity] = useState("current");
 
   useEffect(() => {
     async function getCurrentLocation() {
@@ -24,8 +27,40 @@ const GlobalStore = ({ children }) => {
     getCurrentLocation();
   }, []);
 
+  const getCityWeather = useCallback(
+    async (value) => {
+      setIsLoading(true);
+      if (value === SELECT_OPTIONS_CITIES[0].value) {
+        const { data: weatherData } = await getWeather(
+          location.lat,
+          location.lon
+        );
+        setWeather(weatherData);
+      } else {
+        const { lat, lon } = SELECT_OPTIONS_CITIES.find(
+          (city) => city.value === value
+        );
+        const { data: weatherData } = await getWeather(lat, lon);
+        setWeather(weatherData);
+      }
+      setIsLoading(false);
+    },
+    [location]
+  );
+
+  const changeSelectedCity = useCallback(
+    (value) => {
+      if (value === selectedCity) return;
+      setSelectedCity(value);
+      getCityWeather(value);
+    },
+    [setSelectedCity, getCityWeather, selectedCity]
+  );
+
   return (
-    <GlobalContext.Provider value={{ location, weather, isLoading }}>
+    <GlobalContext.Provider
+      value={{ location, weather, isLoading, selectedCity, changeSelectedCity }}
+    >
       {children}
     </GlobalContext.Provider>
   );
