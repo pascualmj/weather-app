a47fb404a8c6e3052ec526bfc168c6474fcbb830
Manuel Pascual <manupascual90@gmail.com>
2020-07-05 15:56:03 -0300

    Add error handling

--- src/context/GlobalStore.js
+++ src/context/GlobalStore.js
@@ -10,18 +10,25 @@ const GlobalStore = ({ children }) => {
   const [location, setLocation] = useState(null);
   const [weather, setWeather] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [selectedCity, setSelectedCity] = useState("current");
+  const [error, setError] = useState(false);
+  const [selectedCity, setSelectedCity] = useState(
+    SELECT_OPTIONS_CITIES[0].value
+  );
 
   useEffect(() => {
     async function getCurrentLocation() {
       setIsLoading(true);
-      const { data: locationData } = await getLocation();
-      const { data: weatherData } = await getWeather(
-        locationData.lat,
-        locationData.lon
-      );
-      setLocation(locationData);
-      setWeather(weatherData);
+      try {
+        const { data: locationData } = await getLocation();
+        const { data: weatherData } = await getWeather(
+          locationData.lat,
+          locationData.lon
+        );
+        setLocation(locationData);
+        setWeather(weatherData);
+      } catch (error) {
+        setError(true);
+      }
       setIsLoading(false);
     }
     getCurrentLocation();
@@ -30,18 +37,15 @@ const GlobalStore = ({ children }) => {
   const getCityWeather = useCallback(
     async (value) => {
       setIsLoading(true);
-      if (value === SELECT_OPTIONS_CITIES[0].value) {
-        const { data: weatherData } = await getWeather(
-          location.lat,
-          location.lon
-        );
+      try {
+        let latLon = { lat: location.lat, lon: location.lon };
+        if (value !== SELECT_OPTIONS_CITIES[0].value) {
+          latLon = SELECT_OPTIONS_CITIES.find((city) => city.value === value);
+        }
+        const { data: weatherData } = await getWeather(latLon.lat, latLon.lon);
         setWeather(weatherData);
-      } else {
-        const { lat, lon } = SELECT_OPTIONS_CITIES.find(
-          (city) => city.value === value
-        );
-        const { data: weatherData } = await getWeather(lat, lon);
-        setWeather(weatherData);
+      } catch (error) {
+        setError(true);
       }
       setIsLoading(false);
     },
@@ -59,7 +63,14 @@ const GlobalStore = ({ children }) => {
 
   return (
     <GlobalContext.Provider
-      value={{ location, weather, isLoading, selectedCity, changeSelectedCity }}
+      value={{
+        location,
+        weather,
+        isLoading,
+        error,
+        selectedCity,
+        changeSelectedCity,
+      }}
     >
       {children}
     </GlobalContext.Provider>
