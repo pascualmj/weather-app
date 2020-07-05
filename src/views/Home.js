0ae37aebec70f801307727472db4d181eeb6c340
Manuel Pascual <manupascual90@gmail.com>
2020-07-05 15:56:33 -0300

    Add Alert component to Home View

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,17 +4,24 @@ import CitySelection from "../components/CitySelection";
 import WeatherToday from "../components/WeatherToday";
 import WeatherForecast from "../components/WeatherForecast";
 import Preloader from "../components/Preloader";
+import Alert from "../components/commons/Alert";
 
 import { useGlobalStore } from "../hooks";
 
 const Home = () => {
-  const { isLoading } = useGlobalStore();
+  const { isLoading, error } = useGlobalStore();
 
   return (
     <>
       <CitySelection />
       {isLoading ? (
         <Preloader text="Checking the weather..." className="mt-6" />
+      ) : error ? (
+        <Alert
+          text="Something went wrong! Try again later..."
+          type="error"
+          className="mt-6"
+        />
       ) : (
         <>
           <WeatherToday className="mt-6" />
